23890379b4e9f997e7f8acd483fafc1b41153bcb
Chris Frewin <frewin.christopher@gmail.com>
2024-02-18 21:28:16 +0100

    initial commit... all seems to be working

---
+++ README.md
@@ -0,0 +1,79 @@
+# pontificator
+
+Generate studio quality dictated audio files from books, articles, and blog posts.
+
+## Prerequisites
+
+If you are fine with using the built in text-to-speech software, you can skip this section. If you want to use a payed text-to-speech software, you will need to set up an account and get an API key. We currently support the following vendors:
+
+- Eleven Labs. You'll need to set an `ELEVEN_LABS_API_KEY` - if defined in `.env`, the Eleven Labs text-to-speech API will be used, optionally additionally with `ELEVEN_LABS_VOICE_ID`.
+- More coming soon!
+
+
+## Installation
+
+Activate your virtual environment and install the required packages:
+
+```bash
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Example
+
+Paste any markdown into the `example/blog-post.mdx` file. (Or leave as-is for the example.)
+
+Then, run `pontificator` with the following command:
+
+```python
+python3 python/pontificator.py example/blog-post.mdx
+```
+
+This will clean up the markdown, chunk it for API calls, and combine the audio chunks to a single .mp3 file reflecting the entire text content of the blog post.
+
+If all goes well, you should get an `audio.mp3` file produced in the `example` directory.
+
+## Advanced Options for Connoisseurs
+
+## Conversion Examples
+
+### Blog Post
+
+```python
+python3 python/clean_mdx.py example/blog-post.mdx
+```
+
+`blog-post.txt` should be cleaned and ready to be dictated by any text-to-speech software.
+
+### 'Unit Test' Example
+
+```python
+python3 python/clean_mdx.py example/test.mdx
+```
+
+`test.txt` should be cleaned and ready to be dictated by any text-to-speech software.
+
+
+## Chunking Examples
+
+In order to avoid rate limits and max character limits of text-to-speech software, it is recommended to chunk the text into smaller files.
+
+### Blog Post
+
+```python
+python split_into_chunks.py blog-post.txt
+```
+
+This will result in a variety of files with the naming convention `blog-post-chunk-#.txt` where `#` is the chunk number.
+
+## Generate Audio
+
+### Blog Post
+
+```python
+python generate_audio.py blog-post-chunk-1.txt
+```
+
+### Validation and Verification
+
+Consider validating the produced text with CodeVideo's other tool, [`speech-shield`](https://github.com/codevideo/speech-shield)
---
+++ python/generate_audio.py
@@ -0,0 +1,107 @@
+import os
+from dotenv import load_dotenv
+import sys
+import json
+import requests
+import pyttsx3
+
+class VoiceSettings:
+    def __init__(self, stability, similarity_boost):
+        self.stability = stability
+        self.similarity_boost = similarity_boost
+
+class VoiceSettingsEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, VoiceSettings):
+            return obj.__dict__
+        return json.JSONEncoder.default(self, obj)
+
+class TextToSpeechRequest:
+    def __init__(self, text, model_id, voice_settings):
+        self.text = text
+        self.model_id = model_id
+        self.voice_settings = voice_settings
+
+class TextToSpeechRequestEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, TextToSpeechRequest):
+            return {
+                'text': obj.text,
+                'model_id': obj.model_id,
+                'voice_settings': obj.voice_settings.__dict__  # Serialize VoiceSettings separately
+            }
+        return json.JSONEncoder.default(self, obj)
+
+def generate_audio(file_to_chunk, force_overwrite=False):
+    file_no_ext = file_to_chunk.split('.')[0]
+    # Read the text to speak from the file
+    with open(file_to_chunk, 'r', encoding='utf-8') as file:
+        text_to_speak = file.read()
+
+    # get the audio folder path from the file name
+    audio_folder_path = os.path.join(os.getcwd(), file_to_chunk)
+    # If the file exists already, don't do anything
+    audio_file_path = f"{file_no_ext}.mp3"
+    if os.path.exists(audio_file_path) and not force_overwrite:
+        print(f"Audio file already exists. Skipping...")
+        return
+
+    # If API key is defined, use elevenlabs API
+    if 'ELEVEN_LABS_API_KEY' in os.environ:
+        # Headers for elevenlabs
+        headers = {
+            'Content-Type': 'application/json',
+            'xi-api-key': os.environ.get('ELEVEN_LABS_API_KEY', ''),
+            'Accept': 'audio/mpeg',
+        }
+
+        # Create TextToSpeechRequest object
+        body = TextToSpeechRequest(
+            text=text_to_speak,
+            model_id='eleven_turbo_v2',
+            voice_settings=VoiceSettings(stability=0.5, similarity_boost=0.95)
+        )
+
+        try:
+            # Make POST request to elevenlabs
+            response = requests.post(
+                f"https://api.elevenlabs.io/v1/text-to-speech/{os.environ.get('ELEVEN_LABS_VOICE_ID', '')}",
+                headers=headers,
+                data=json.dumps(body, cls=TextToSpeechRequestEncoder)  # Use the custom encoder
+            )
+
+            response.raise_for_status()
+
+            # Write the byte data to an MP3 file
+            with open(audio_file_path, 'wb') as file:
+                file.write(response.content)
+            
+            print(f"Audio file {audio_file_path} generated successfully.")
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+            return None
+
+    # If API key not defined, use pyttsx3 to do text-to-speech
+    else:
+        engine = pyttsx3.init()
+        engine.save_to_file(text_to_speak, audio_file_path)
+        engine.runAndWait()
+        print("Audio file generated successfully.")
+
+def load_env_variables():
+    dotenv_path = '.env'  # Path to your .env file
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
+
+if __name__ == "__main__":
+    # Load environment variables
+    load_env_variables()
+    
+    if len(sys.argv) != 2:
+        print("Usage: python generate_audio.py file_to_chunk.txt")
+        sys.exit(1)
+
+    file_to_chunk = sys.argv[1]
+
+    generate_audio(file_to_chunk, force_overwrite=False)
